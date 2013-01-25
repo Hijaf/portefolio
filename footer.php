9f528c29c9e2903025b139402bf48a825b68ff18
Kevin <kevin.gueders@gmail.com>
2013-01-25 22:26:14 +0100

    Intégration Boilerplate

--- footer.php
+++ footer.php
@@ -4,5 +4,9 @@
 					PorteFolio réalisé par Kévin Guéders - Tout droits réservés
 			</p>
 		</footer>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+        <script src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/js/plugins.js"></script>
+        <script src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/js/main.js"></script>
     </body>
 </html>
--- header.php
+++ header.php
@@ -1,17 +1,26 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo('charset');?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="description" content="<?php bloginfo('dexcription');?>">
-	<script src="http://use.edgefonts.net/league-gothic.js"></script>
+	<meta name="viewport" content="width=device-width; initial-scale=1.0">
 	<link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet" type="text/css">
 	<title><?php bloginfo('name');?></title>
-	<?php wp_head();?>
+	<link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/css/normalize.css">
+    <link rel="stylesheet" href="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/css/main.css">
+    <script src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/js/vendor/modernizr-2.6.2.min.js"></script>
 </head>
 <body>
+<!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->
 <div id="centre">
 			<header>
-					<h1><a href="#accueil" title="vers l'accueil"><img src="http://localhost/wordpress/wp-content/themes/portefolio/img/LogoGkeck1.png" height="76" width="210" alt="GKeck Web Design - Web Development"/></a></h1>
+					<h1><a href="<?php echo get_site_url(); ?>" title="vers l'accueil"><img src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/img/LogoGkeck1.png" height="76" width="210" alt="GKeck Web Design - Web Development"/></a></h1>
 				<nav>
 					<?php wp_nav_menu('header-menu');?>
 				</nav>
