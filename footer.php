332cc8b02bceeef3ef02bb0b04018f19d80d05c9
Kevin <kevin.gueders@gmail.com>
2013-02-03 23:51:04 +0100

    requete inutile

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
 		</footer>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
-        <script src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/js/plugins.js"></script>
         <script src="<?php echo get_site_url(); ?>/wp-content/themes/portefolio/js/main.js"></script>
     </body>
 </html>
